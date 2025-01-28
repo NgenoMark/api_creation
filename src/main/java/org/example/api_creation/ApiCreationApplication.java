333e1ba02521b2ff78df1e312ed0e383468a2540
NgenoMark <kipchumbamark48@gmail.com>
2025-01-28 12:06:18 +0300

    Initial commit

---
+++ src/main/java/org/example/api_creation/ApiCreationApplication.java
@@ -0,0 +1,13 @@
+package org.example.api_creation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCreationApplication.class, args);
+	}
+
+}
